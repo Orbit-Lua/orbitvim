8ab3623355d91e5f9ae6fa669fc65362ce453e42
abner <qw0207060413@gmail.com>
2026-08-10 01:00:56 +0800

    fix(test): remove 'integration not nil' rule from nvui init spec due to disable todo integration

--- lua/test/spec/config_init_spec.lua
+++ lua/test/spec/config_init_spec.lua
@@ -17,7 +17,7 @@ describe("config", function()
     end)
 
     it("exposes the remaining local Nv UI settings", function()
-      assert.is_not_nil(chadrc.base46.integrations)
+      -- assert.is_not_nil(chadrc.base46.integrations)
       assert.is_not_nil(chadrc.colorify)
       assert.is_function(chadrc.ui.statusline.ai.is_available)
     end)
